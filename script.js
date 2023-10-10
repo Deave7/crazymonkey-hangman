22fe8e35a3017e1ce0d041425e40c914bcf16f2a
Maja Wennstam <majawennstam@bahnhof.se>
2023-10-10 15:28:10 +0200

    update svg function

---
+++ script.js
@@ -0,0 +1,18 @@
+
+function startGame(){
+    
+const head = document.getElementById("head");
+head.style.display = "none";
+
+}
+
+
+
+
+function hangmansvg(params) {
+    if (guessIncorrect) {
+        /* ta fram en del av svg */
+    } else {
+        
+    }
+}
