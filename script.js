3a9dc379a7f9687937cde8d987f425619a5b107a
Gustav Syrén <g.e.syren@gmail.com>
2023-10-10 11:32:53 +0200

    readme & javascript update

---
+++ script.js
@@ -0,0 +1,18 @@
+//Slumpfunktionen 
+
+//tangentbords-input
+
+//*knapp-input
+
+//skriv ut resultat per gissning (använda bokstäver etc)
+
+//Justerar bilden 
+
+//vinst/förlust
+
+
+
+//* Meny - tillåta lokal multiplayer 
+
+//* hint? slumpa fram en bokstav ur rätt ord
+
