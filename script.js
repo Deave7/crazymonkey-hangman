f040c37cdcf822aa8cc3dd46966120093a45787b
ossian igelberg <ossian.igelberg@gmail.com>
2023-10-10 15:33:42 +0200

    win-loss edit

--- script.js
+++ script.js
@@ -16,3 +16,24 @@
 
 //* hint? slumpa fram en bokstav ur rätt ord
 
+
+
+
+
+/* document.getElementById("lettersGuessed").innerHTML += lettersGuessed;
+document.getElementById("guessesLeft").innerHTML = guessesLeft;
+
+  guessesLeft--;
+
+  if (guessesLeft === -1) {
+    alert("You Loose!");
+  } */
+
+// 
+
+if (progressWord.indexOf('_') == -1 ){ // if there are no dashes left
+    //win!
+  }else if (guessesLeft <= 0){
+    //player has lost
+  }
+
